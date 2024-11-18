4345d896192b533bef3441a5c6ce26c2ee6d6dc4
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2024-11-18 23:27:45 +0800

    0821

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,52 @@
+import {defineConfig} from 'vitepress'
+//import AutoSidebar from 'vite-plugin-vitepress-auto-sidebar';
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+    title: "WJZ_P的个人博客",
+    description: "By WJZ_P",
+    head: [
+        ['link', {rel: 'icon', href: `/smile.png`}]
+    ],
+    // vite: {
+    //     plugins: [
+    //         //添加插件
+    //         AutoSidebar({
+    //             //插件的配置选项，具体在https://github.com/QC2168/vite-plugin-vitepress-auto-sidebar
+    //         })
+    //     ]
+    // },
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        logo: "/smile.jpg",
+        search: {
+            provider: 'local'
+        },
+        nav: [
+            {text: '主页', link: '/'},
+            {text: 'Examples', link: '/markdown-examples'}
+        ],
+
+        sidebar: [
+            {
+                text: '个人介绍',
+                items: [
+                    {text: '简介', link: '/introduction/自我介绍'},
+                ]
+            },
+            {
+                text: '其他',
+                items: [
+                    {text: '杂项', link: '/others/其他'},
+                ]
+            },
+        ],
+
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/WJZ-P'}
+        ],
+        footer: {
+            message: 'wow！',
+            copyright: '2022.03.13 @WJZ_P',
+        }
+    }
+})
