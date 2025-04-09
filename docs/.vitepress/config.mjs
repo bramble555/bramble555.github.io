d46e5dff406385807b9e731291363d48b86e4b26
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2025-04-09 13:38:25 +0800

    0821

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -37,30 +37,31 @@ export default defineConfig({
                 text: '编程相关',
                 items: [
                     {text: '专业术语', link: '/coding/专业术语'},
-                    {text: '力扣好题',link: '/coding/LeetCode好题'},
-                    {text: 'Java源码',link: '/coding/Java源码'},
-                    {text: 'Java细节',link:'/coding/Java细节'},
+                    {text: '力扣好题', link: '/coding/LeetCode好题'},
+                    {text: 'Java源码', link: '/coding/Java源码'},
+                    {text: 'Java细节', link: '/coding/Java细节'},
 
                 ]
             },
             {
                 text: 'MySQL笔记',
                 items: [
-                    {text: '基础',link:'/MySQL/基础'},
-                    {text: '索引',link:'/MySQL/索引'},
-                    {text: 'B+树',link:'/MySQL/为什么MySQL用B+树'},
-                    {text: '事务',link:'/MySQL/事务'},
-                    {text: '锁篇',link:'/MySQL/锁篇'},
-                    {text: '日志',link:'/MySQL/日志'},
-                    {text: '内存',link:'/MySQL/内存'},
-                    {text: '面试',link:'/MySQL/面试'},
+                    {text: '基础', link: '/MySQL/基础'},
+                    {text: '索引', link: '/MySQL/索引'},
+                    {text: 'B+树', link: '/MySQL/为什么MySQL用B+树'},
+                    {text: '事务', link: '/MySQL/事务'},
+                    {text: '锁篇', link: '/MySQL/锁篇'},
+                    {text: '日志', link: '/MySQL/日志'},
+                    {text: '内存', link: '/MySQL/内存'},
+                    {text: '面试', link: '/MySQL/面试'},
                 ]
             },
             {
                 text: '计算机网络笔记',
                 items: [
-                    {text: '基础',link:'/network/计网基础'},
-                    {text: 'HTTP',link:'/network/HTTP篇'},
+                    {text: '基础', link: '/network/计网基础'},
+                    {text: 'HTTP', link: '/network/HTTP篇'},
+                    {text: '面试篇', link: '/network/面试篇'},
 
                 ]
             },
