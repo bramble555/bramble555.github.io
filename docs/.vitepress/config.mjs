6391a597149e7a043085e0f6ba2de7c4cdb58efa
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2025-04-21 21:55:14 +0800

    0821

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -115,9 +115,10 @@ export default defineConfig({
             {
                 text: '我的面试经历',
                 items: [
-                    {text: '1.字节国际电商', link: '/myInterview/第一次面试-字节国际电商'},
-                    {text: '2.广州华资软件', link: '/myInterview/第二次面试-广州华资软件公司'},
-                    {text: '3.腾讯元宝', link: '/myInterview/第三次面试-腾讯云宝开发'},
+                    {text: '1.字节国际电商', link: '/myInterview/1.第一次面试-字节国际电商'},
+                    {text: '2.广州华资软件', link: '/myInterview/2.第二次面试-广州华资软件公司'},
+                    {text: '3.腾讯元宝', link: '/myInterview/3.第三次面试-腾讯云宝开发'},
+                    {text: '3.腾讯元宝', link: '/myInterview/4.第四次面试-字节懂车帝后端'},
                 ]
             },
             {
